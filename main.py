b2dad6e59f59b5ab6d28beffb09bdb4334c5248f
surajalivexd <surajkumar14dec@gmail.com>
2026-06-10 19:37:45 +0530

    removed commas etc

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello")
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r"C:\Users\suraj\Documents\suraj doc\bank_statement.csv")
+df["Amount"]=df["Amount"].astype(str).replace('"',"").replace (",",'').str.strip()
+print(df)
